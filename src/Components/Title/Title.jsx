1eb6270f7e41e62e32a3a698e0bbc89ed49ccd91
editacs <154480881+editacs@users.noreply.github.com>
2024-06-07 16:08:17 -0700

    Update Title.jsx

--- src/Components/Title/Title.jsx
+++ src/Components/Title/Title.jsx
@@ -1,17 +1,12 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';  // Ensure Helmet is imported
-import './Title.css';
+import './Title.css'
 
-const Title = ({ subTitle, title }) => {
+const Title = ({subTitle, title}) => {
   return (
     <div className='title'>
-      <Helmet>
-        <title>{title} - Tow Tech Solutions</title>  // Dynamically set the document title
-      </Helmet>
       <p>{subTitle}</p>
       <h2>{title}</h2>
     </div>
-  );
+  )
 }
 
-export default Title;
+export default Title
