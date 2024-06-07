22c3106be54e811118bb5951200b2c414724912f
editacs <154480881+editacs@users.noreply.github.com>
2024-06-07 16:35:25 -0700

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import React from 'dux` from 'react';
+import React from 'react';
 import './Hero.css';
 import dark_arrow from '../../assets/dark-arrow.png';
 
@@ -8,7 +8,7 @@ const Hero = () => {
       <div className="hero-text">
         <h1>Experience top-tier roadside assistance with TowTech.</h1>
         <p>Our team delivers prompt, reliable service 24/7. Count on us for transparent pricing and customer satisfaction.</p>
-        <a href="https://www.instagram.com/towtech_solutions/" className='btn'>
+        <a href="https://www.tiktok.com/@towtech.solutions" className='btn'>
           Explore more <img src={dark_arrow} alt="Navigate" />
         </a>
       </div>
@@ -17,4 +17,3 @@ const Hero = () => {
 }
 
 export default Hero;
-
