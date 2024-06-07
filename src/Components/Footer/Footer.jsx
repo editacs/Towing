3cedaffc1f83b5988abad421cf1b27a53ffdc8ba
editacs <154480881+editacs@users.noreply.github.com>
2024-06-06 22:28:06 -0700

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,11 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-      <p>© 2024 TowTech Solutions. All rights reserved.</p>
-      <ul>
-        <li><a href="https://www.youtube.com/@GreatStackDev/videos" target='_blank'>Terms of Services</a></li>
-        <li><a href="https://www.youtube.com/@GreatStackDev/videos" target='_blank'>Privacy Policy</a></li>
-      </ul>
+      <p>© 2024 TowTech Solutions. All rights reserved.</p>  
     </div>
   )
 }
