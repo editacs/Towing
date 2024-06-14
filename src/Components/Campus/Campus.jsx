c95d3baaf19950f8eec127b9d27509d8b885f5de
editacs <154480881+editacs@users.noreply.github.com>
2024-06-14 13:01:40 -0700

    Update Campus.jsx

--- src/Components/Campus/Campus.jsx
+++ src/Components/Campus/Campus.jsx
@@ -5,31 +5,19 @@ import gallery_2 from '../../assets/gallery-2.png'
 import gallery_3 from '../../assets/gallery-3.png'
 import gallery_4 from '../../assets/gallery-4.png'
 import white_arrow from '../../assets/white-arrow.png'
-import img_6013 from '../../assets/IMG_6013.jpg'
-import img_6517 from '../../assets/IMG_6517.jpg'
-import img_6115 from '../../assets/IMG_6115.jpg'
-import img_6612 from '../../assets/IMG_6612.jpg'
 
 const Campus = () => {
   return (
     <div className='campus'>
       <div className="gallery">
-        <img src={gallery_1} alt="Gallery 1" />
-        <img src={gallery_2} alt="Gallery 2" />
-        <img src={gallery_3} alt="Gallery 3" />
-        <img src={gallery_4} alt="Gallery 4" />
+        <img src={gallery_1} alt="" />
+        <img src={gallery_2} alt="" />
+        <img src={gallery_3} alt="" />
+        <img src={gallery_4} alt="" />
       </div>
-      <a href="#picture-section" className='btn dark-btn'>See more here <img src={white_arrow} alt="Arrow" /></a>
-
-      <div id="picture-section" className="picture-section">
-        <h2>More Pictures</h2>
-        <img src={img_6013} alt="IMG 6013" />
-        <img src={img_6517} alt="IMG 6517" />
-        <img src={img_6115} alt="IMG 6115" />
-        <img src={img_6612} alt="IMG 6612" />
-      </div>
+      <button className='btn dark-btn'>See more here <img src={white_arrow} alt="" /></button>
     </div>
   )
 }
 
-export default Campus
+export default
